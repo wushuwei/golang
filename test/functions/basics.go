1428078a694f828cb492738771f273361964793b
Shu Wei Wu <wushuwei@gmail.com>
2021-03-20 23:17:32 -0400

    v1

--- test/functions/basics.go
+++ test/functions/basics.go
@@ -1,11 +1,42 @@
 package functions
-import(
+
+import (
 	"fmt"
 )
 
-func HelloWorld(){
-	str := "world"
-	_, err := fmt.Printf("Type: %T\n", str)
-	if err != nil {
+func HelloWorld() {
+	// str := "world"
+	// fmt.Printf("Type: %T\n", str)
+
+	// const y = "hello"
+	// fmt.Printf("Type: %T\n", y)
+
+	// x := [3]int{10, 20, 30}
+	// fmt.Printf("Type: %T\n", x)
+
+	// z := [...]int{10, 20, 30}
+	// fmt.Printf("Type: %T\n", z)
+
+	m := make([]int, 0, 10)
+	fmt.Printf("Type: %T\n", m)
+	m = append(m, 5, 6, 7, 8)
+	fmt.Printf("Type: %T\n", m)
+	fmt.Printf("Values: %v\n", m)
+
+	t := map[string][]string{
+		"Orcas":   {"Fred", "Ralph", "Bijou"},
+		"Lions":   {"Sarah", "Peter", "Billie"},
+		"Kittens": {"Waldo", "Raul", "Ze"},
 	}
+	fmt.Printf("Type: %T\n", t)
+	fmt.Printf("Values: %v\n", t)
+
+	for _, v := range t {
+		fmt.Printf("v only: %v\t", v)
+	}
+
+	if _, ok := t["Lions"]; ok {
+		fmt.Println("found!")
+	}
+
 }
