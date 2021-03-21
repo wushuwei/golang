58ee8fdb908b7d20b4399676db346442ecde8d4c
Shu Wei Wu <wushuwei@gmail.com>
2021-03-21 00:17:44 -0400

    gen test

---
+++ test/functions/basics_test.go
@@ -0,0 +1,56 @@
+package functions
+
+import (
+	"testing"
+)
+
+func TestPointers(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Pointers()
+		})
+	}
+}
+
+func TestFunctions(t *testing.T) {
+	type args struct {
+		i int
+		j int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		want1   int
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			"test1",
+			args{0, 0},
+			0,
+			0,
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := Functions(tt.args.i, tt.args.j)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Functions() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Functions() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("Functions() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
